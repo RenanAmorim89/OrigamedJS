eae5e4180a370b404e49b1a90d064d627c4f92fb
Renan Amorim <renanamorim89@hotmail.com>
2023-04-19 08:02:05 -0300

    variaveis

---
+++ javascript para inciantes/1 - variaveis/exercicio string/script.js
@@ -0,0 +1,12 @@
+// Declarar uma variável com o seu nome
+let nome = "Renan";
+// Declarar uma variável com a sua idade
+let idade = 33;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita =
+  // Atribuir valor a sua comida favorita
+  (comidaFavorita = "Carne assada");
+// Declarar 5 variáveis diferentes sem valores
+
+let carro, moto, casa, animal, caneta;
---
+++ javascript para inciantes/1 - variaveis/script.js
@@ -0,0 +1,28 @@
+let nome = "Renan";
+let idade = 33;
+const possuiFaculdade = true;
+console.log(nome, idade, possuiFaculdade);
+
+let preco = 20;
+let totalComprado = 5;
+let precoTotal = preco * totalComprado;
+console.log(totalComprado);
+
+let nome1 = "Renan",
+  idade1 = 34,
+  possuiFaculdade1 = true;
+console.log(nome1, idade1, possuiFaculdade1);
+
+let precoAplicativo;
+//retona undefinid
+
+/* Inválido
+var §nome;
+var function;
+var 1possuiFaculdade;
+
+// Válido
+var $selecionar;
+var _nome;
+var possuiFaculdadeNoExterior;
+*/
