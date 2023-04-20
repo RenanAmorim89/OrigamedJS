86fc0eb08f3a502e47097617e3316bcb8bf6e265
Renan Amorim <renanamorim89@hotmail.com>
2023-04-20 07:47:22 -0300

    numeros e operadores-exercicios

---
+++ javascript para inciantes/3 -numeros e operadores/exercicio/script1.js
@@ -0,0 +1,24 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + (5 * 2) / 2 + 20; // 35
+
+// Crie duas expressões que retornem NaN
+let nome = "Renan 10";
+let idade = 23;
+let idadeCerta = nome / idade;
+console.log(idadeCerta);
+// Somar a string '200' com o número 50 e retornar 250
+let numero1 = +"200";
+let numero2 = 50;
+console.log(numero1 + numero2);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+let numero3 = 5;
+console.log(++numero3);
+
+// Como dividir o peso por 2?
+var numero = +"80" / 2;
+var unidade = "kg";
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso; // NaN (Not a Number)
+
+console.log(pesoPorDois);
