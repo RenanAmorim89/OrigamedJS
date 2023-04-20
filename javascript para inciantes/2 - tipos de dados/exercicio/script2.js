5cc8680f9aac64ecf68d70c3acec18266736e19a
Renan Amorim <renanamorim89@hotmail.com>
2023-04-20 06:21:44 -0300

    tipos de dados-exercicio

---
+++ javascript para inciantes/2 - tipos de dados/exercicio/script2.js
@@ -0,0 +1,20 @@
+// Declare uma variável contendo uma string
+let nomeCachorro = "Leitão";
+
+// Declare uma variável contendo um número dentro de uma string
+let numeroCasa = "445";
+
+// Declare uma variável com a sua idade
+let idade = 33;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+let nome = "Renan";
+let sobrenome = "Amorim";
+let nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+// Coloque a seguinte frase em uma variável: It's time
+let frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
