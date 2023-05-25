423a03289e72d314f52fe9bbc6d23c1e767dfc9b
Renan Amorim <renanamorim89@hotmail.com>
2023-05-25 14:12:27 -0300

    exercicio-function

---
+++ 03 - OBJETOS/08 - Function/exercicio/script.js
@@ -0,0 +1,40 @@
+// Retorne a soma total de caracteres dos
+// parágrafos acima utilizando reduce
+
+const paragrafos = document.querySelectorAll('p')
+
+let arrayParagrafos =  Array.from(paragrafos)
+
+const totalparagrafos = arrayParagrafos.reduce((acumulador, item)=>{
+  return acumulador + item.innerText.length
+},0)
+
+console.log(totalparagrafos)
+
+
+
+
+// Crie uma função que retorne novos elementos
+// html, com os seguintes parâmetros
+// tag, classe e conteudo.
+
+function criarElemento(tag, classe, conteudo){
+  const elemento = document.createElement(tag)
+  classe ? elemento.classList.add(classe) : null
+  conteudo ? elemento.innerHTML = conteudo : null
+  return elemento 
+}
+
+console.log(criarElemento('h1', 'titulo', 'esse é o conteudo'))
+
+
+// Crie uma nova função utilizando a anterior como base
+// essa nova função deverá sempre criar h1 com a
+// classe titulo. Porém o parâmetro conteudo continuará dinâmico
+
+const h1Titulo = criarElemento.bind(null, 'h1', 'titulo')
+
+const cursosJS = h1Titulo('Cursos de JS')
+const cursosHTML = h1Titulo('Cursos de HTML')
+
+console.log(cursosJS, cursosHTML)
