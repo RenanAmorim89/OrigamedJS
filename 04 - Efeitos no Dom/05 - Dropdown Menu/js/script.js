6650983fbe387080052970e2e8cfb8302fc8a509
Renan Amorim <renanamorim89@hotmail.com>
2023-06-06 08:49:00 -0300

    dropdown-menu

---
+++ 04 - Efeitos no Dom/05 - Dropdown Menu/js/script.js
@@ -0,0 +1,25 @@
+import initScrollSuave  from  './modules/scroll-suave.js'
+
+import initAnimacaoScroll from './modules/scroll-animacao.js'
+
+import initAccordion from './modules/accordion.js'
+
+import initTabNav from './modules/tabNav.js'
+
+import initModal from './modules/modal.js'
+
+import initTooltip from './modules/tooltip.js'
+
+import initDropDownMenu from './modules/dropdown-menu.js'
+
+// import outsideClick from './modules/outsideclick.js'
+
+initScrollSuave()
+initAnimacaoScroll()
+initAccordion()
+initTabNav()
+initModal()
+initTooltip()
+initDropDownMenu()
+// outsideClick()
+
